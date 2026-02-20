85ceb565e2a30d7f39503cfb9a0a33612d996e79
vijay <vijayasankarkambala008@gmail.com>
2026-02-20 21:41:51 +0530

    post data to backend

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,14 +1,36 @@
 import '../styles/pages/Signup.css';
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
 function Signup() {
+
+    const handleSignup = (e) => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const phoneNo = e.target.phone.value;
+        const password = e.target.password.value;
+
+        console.log(name ,email, phoneNo ,password);
+
+        axios.post("http://localhost:9090/customers",{
+            name,
+            email,
+            phoneNo,
+            password
+        }).then(response => {
+            console.log(response.data);
+        }).catch(error =>{
+            console.log(error)
+        })
+    }
     return (
         <div className="fullscreen-wrapper">
             <div className="signup-container">
                 <div className="logo-area">
                     <h2>Create Account</h2>
                 </div>
-                <form>
+                <form onSubmit={handleSignup}>
                     <div className="form-group">
                         <label htmlFor="name">Full Name</label>
                         <input type="text" id="name" placeholder="John Doe" required />
